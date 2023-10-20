8cca42cbf20053d2939ba9a3be221f02240eb8de
M V K <krishna.mugala9@gmail.com>
2023-10-20 17:41:47 +0530

    project structure created

---
+++ pokemon_Backend/src/main/java/com/pokemon/pokemon_Backend/PokemonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.pokemon_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonBackendApplication.class, args);
+	}
+
+}
---
+++ pokemon_Backend/src/test/java/com/pokemon/pokemon_Backend/PokemonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.pokemon_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
