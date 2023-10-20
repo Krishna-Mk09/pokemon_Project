206d45c3975639c7c63ce5777261f03afa82cff6
M V K <krishna.mugala9@gmail.com>
2023-10-20 21:34:56 +0530

    added attributes and dependencies in backend

---
+++ pokemon_Backend/src/main/java/com/pokemon/pokemon_Backend/domain/Pokemon.java
@@ -0,0 +1,23 @@
+package com.pokemon.pokemon_Backend.domain;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Pokemon {
+    @Id
+    private int id;
+    private String name;
+    private int height;
+    private int weight;
+    private String url;
+
+
+}
