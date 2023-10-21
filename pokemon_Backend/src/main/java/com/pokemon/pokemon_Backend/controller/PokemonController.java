34b76dea73804cdcd6fd275092cae9c54a581860
M V K <krishna.mugala9@gmail.com>
2023-10-21 06:47:24 +0530

    Added restController, request mapping annotations and autowired it

--- pokemon_Backend/src/main/java/com/pokemon/pokemon_Backend/controller/PokemonController.java
+++ pokemon_Backend/src/main/java/com/pokemon/pokemon_Backend/controller/PokemonController.java
@@ -1,4 +1,17 @@
 package com.pokemon.pokemon_Backend.controller;
 
+import com.pokemon.pokemon_Backend.service.PokemonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pokemon")
 public class PokemonController {
+    private final PokemonService POKEMON_SERVICE;
+
+    @Autowired
+    public PokemonController(PokemonService pokemonService) {
+        POKEMON_SERVICE = pokemonService;
+    }
 }
