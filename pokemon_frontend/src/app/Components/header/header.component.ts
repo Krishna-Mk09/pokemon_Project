d7f303ee2f6180b1ec17b21aea2e53eb534aed7f
M V K <krishna.mugala9@gmail.com>
2023-10-22 08:43:52 +0530

    changes made in pgination part of dashboard

--- pokemon_frontend/src/app/Components/header/header.component.ts
+++ pokemon_frontend/src/app/Components/header/header.component.ts
@@ -7,39 +7,8 @@ import {PokemonServiceService} from "../../services/pokemon-service.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent  {
-  filteredPokemons: any[] = [];
-  pokemons: any[] = [];
-  searchQuery: string = '';
 
-  constructor(private service: PokemonServiceService) {
-  }
 
-  // search() {
-  //   if (this.searchQuery == "") {
-  //     this.ngOnInit();
-  //   } else {
-  //     this.pokemons = this.pokemons.filter(res => {
-  //       return res.name?.toLowerCase().match(this.searchQuery.toLocaleLowerCase());
-  //     })
-  //   }
-  // }
-
-  // ngOnInit(): void {
-  //   this.service.getPokemons().subscribe((response: any) => {
-  //
-  //     if (Array.isArray(response.results)) {
-  //       response.results.forEach((element: any) => {
-  //         const pokemonName = element.name; // Get the name from the response
-  //         this.service.getMoreData(pokemonName).subscribe((res: any) => {
-  //           this.pokemons.push(res);
-  //           console.log(this.pokemons);
-  //         });
-  //       });
-  //     } else {
-  //       console.error('Response does not contain an array of results:', response);
-  //     }
-  //   });
-  // }
 }
 
 
--- pokemon_frontend/src/app/Components/pokemon-dashboard/pokemon-dashboard.component.ts
+++ pokemon_frontend/src/app/Components/pokemon-dashboard/pokemon-dashboard.component.ts
@@ -11,22 +11,20 @@ export class PokemonDashboardComponent implements OnInit {
   filteredPokemons: any[] = [];
   searchQuery: string = '';
   p:number=1;
-  itemsPerPage:number=8
-  totalProduct:any;
-
-
+  itemsPerPage:number=5
+  totalPokemon:any;
 
   constructor(private service:PokemonServiceService){}
+
   ngOnInit(): void {
     this.service.getPokemons().subscribe((response: any) => {
-
       if (Array.isArray(response.results)) {
         response.results.forEach((element: any) => {
           const pokemonName = element.name; // Get the name from the response
           this.service.getMoreData(pokemonName).subscribe((res: any) => {
             this.pokemons.push(res);
             console.log(this.pokemons);
-            this.totalProduct=response.result.length;
+
           });
         });
       } else {
@@ -47,38 +45,5 @@ export class PokemonDashboardComponent implements OnInit {
       })
     }
   }
-//   pokemons: any[] = [];
-//   filteredPokemons: any[] = [];
-//   searchQuery: string = '';
-//
-//   constructor(private service: PokemonServiceService) {
-//   }
-//
-//
-//   ngOnInit(): void {
-//     this.service.getPokemons().subscribe((response: any) => {
-//       if (Array.isArray(response.results)) {
-//         response.results.forEach((element: any) => {
-//           const pokemonName = element.name; // Get the name from the response
-//           this.service.getMoreData(pokemonName).subscribe((res: any) => {
-//             this.pokemons.push(res);
-//             console.log(this.pokemons);
-//           });
-//         });
-//       } else {
-//         console.error('Response does not contain an array of results:', response);
-//       }
-//     });
-//   }
-//
-//
-//   search() {
-//     if (this.searchQuery == "") {
-//       this.ngOnInit();
-//     } else {
-//       this.pokemons = this.pokemons.filter(res => {
-//         return res.name?.toLowerCase().match(this.searchQuery.toLocaleLowerCase());
-//       })
-//     }
-//   }
+
 }
