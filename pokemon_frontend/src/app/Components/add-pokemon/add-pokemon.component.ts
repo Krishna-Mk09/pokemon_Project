b5342725bc19f47aa6d7ac2d439b28300e2ac07f
M V K <krishna.mugala9@gmail.com>
2023-10-27 12:37:09 +0530

    added a component add pokemon in components folder

---
+++ pokemon_frontend/src/app/Components/add-pokemon/add-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pokemon',
+  templateUrl: './add-pokemon.component.html',
+  styleUrls: ['./add-pokemon.component.css']
+})
+export class AddPokemonComponent {
+
+}
